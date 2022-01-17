297a7e4cdf851fb1dd39caf5b8fcdf55fca9bed6
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-16 21:49:00 -0800

    Add prop to load correct badge color

--- components/Badge.js
+++ components/Badge.js
@@ -2,13 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 export default function Badge({ style, day, big, i }) {
-  const animate = i === 4 || day === 365
+  const animate = i === 4 || day === 12 || day === 32 || day === 57 || day === 87 || day === 365
   const background = style
-    return (
-      <div className={`  xs: h-10 w-10 sm: h-18 w-18 p-6 border-dashed m-1 border-2 ${background} rounded-full shadow-xl flex justify-center items-center ${animate && 'animate-bounce'}`}>
-        <p className={`font-bold text-2xl`}>{day}</p>
-      </div>
-    )
+  return (
+    <div className={`  xs: h-10 w-10 sm: h-18 w-18 p-6 border-dashed m-1 border-2 ${background} rounded-full shadow-xl flex justify-center items-center ${animate && 'animate-bounce'}`}>
+      <p className={`font-bold text-2xl`}>{day}</p>
+    </div>
+  )
 }
 
 Badge.propTypes = {
--- components/HabitCard.js
+++ components/HabitCard.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Badge from './Badge'
+import { colors } from '../pages/data/colors';
 
 export default function HabitCard({ title, reason, resource, length, resourceLink }) {
+  const background;
+  const shades = colors.forEach((color, i) => {
+    const shade = color.shades.find(shade => shade.day === length)
+    if(shade){
+      background = shade.shade;
+      console.log(background);
+    }
+  })
   return (
-    <article className=' bg-primaryBlack rounded-xl my-4 px-8 py-4 shadow-2xl'>
+    <article className=' bg-primaryBlack rounded-xl my-8 px-8 py-4 shadow-2xl'>
       <h2 className='text-white font-bold text-3xl text-center'>{title}</h2>
       <h2 className='text-white font-bold text-xl'>Reasons</h2>
       <ul>
         <li className='list-disc text-white m-2'>{reason}</li>
       </ul>
       <h2 className='text-white font-bold text-xl'>Resources</h2>
-    <a href={resourceLink} target='_blank' className='underline decoration-primaryRed text-white'>{resource}</a>
+      <a href={resourceLink} target='_blank' className='underline decoration-primaryRed text-white'>{resource}</a>
       <h2 className='text-white font-bold text-xl'>Current Streak</h2>
       <p className='text-primaryRed font-bold text-xl'>{length}</p>
 
-      <h2 className='text-white font-bold text-xl'>Badges Earned</h2>
-      <hr className='border-b-2 border-primaryRed mb-4'/>
-      <Badge day={length} />
+      <h2 className='text-white font-bold text-xl'>Highest Badge Earned</h2>
+      <hr className='border-b-2 border-primaryRed mb-4' />
+      <Badge day={length} style={background && background} />
     </article>
   )
 }
