a0c5f179441e3d3226e1b16ce8b3c2b4627d8c17
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-19 22:17:01 -0800

    convert button to typescript

---
+++ components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type ButtonProps = {
+  children: React.ReactNode;
+};
+
+export default function Button({ children } : ButtonProps) {
+  return (
+    <button
+      type="submit"
+      className="hover:bg-red-600 font-bold text-2xl bg-primaryRed text-white my-4 py-2 rounded-md border-2-primaryRed shadow-2xl"
+    >
+      {children}
+    </button>
+  );
+}
