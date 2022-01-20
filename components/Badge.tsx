4243e0f21ee5705809a580706c89d5ead085e307
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-19 22:38:48 -0800

    Convert badge to typescript

---
+++ components/Badge.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+type BadgeProps = {
+  style: string;
+  day: number;
+};
+
+export default function Badge({ style, day } : BadgeProps) {
+  // Animation Logic For Specific days
+  const animate =
+    day === 12 ||
+    day === 32 ||
+    day === 57 ||
+    day === 87 ||
+    day === 122 ||
+    day === 162 ||
+    day === 206 ||
+    day === 256 ||
+    day === 311 ||
+    day === 360 ||
+    day === 365;
+
+  return (
+    <div
+      className={`xs: h-10 w-10 sm: h-18 w-18 p-6 border-dashed m-1 border-2 ${style} rounded-full shadow-xl flex justify-center items-center ${
+        animate && "animate-bounce"
+      }`}
+    >
+      <p className={`font-bold text-2xl`}>{day}</p>
+    </div>
+  );
+}
+
