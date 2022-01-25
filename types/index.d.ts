8efc1cd4933fbc211a802f451cb03ec0da3ecd3d
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-24 20:05:48 -0800

    Edit ts files

---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+declare module '@material-tailwind/react/Button'
+declare module "@material-tailwind/react/Card"
+declare module "@material-tailwind/react/Input"
+declare module "@material-tailwind/react/Navbar";
+declare module "@material-tailwind/react/NavbarContainer";
+declare module "@material-tailwind/react/NavbarWrapper";
+declare module "@material-tailwind/react/NavbarToggler";
+declare module "@material-tailwind/react/NavbarCollapse";
+declare module "@material-tailwind/react/Nav";
+declare module "@material-tailwind/react/NavItem";
